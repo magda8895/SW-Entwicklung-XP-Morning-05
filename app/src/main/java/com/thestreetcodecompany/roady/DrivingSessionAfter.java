0f5b799ae972abaddaeb0cf8e939d3ebfdca83e4
Florijan <klezin.florijan95@gmail.com>
2018-04-11 11:43:35 +0200

    [FK,SS] Add new driving session afterwards backend Added time and date picker and array of weather and road conditions

--- app/src/main/java/com/thestreetcodecompany/roady/DrivingSessionAfter.java
+++ app/src/main/java/com/thestreetcodecompany/roady/DrivingSessionAfter.java
@@ -1,20 +1,58 @@
 package com.thestreetcodecompany.roady;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class DrivingSessionAfter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Context c = this;
         setContentView(R.layout.activity_driving_session_after);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        Spinner weatherSpinner = findViewById(R.id.weatherSpinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.Weather, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weatherSpinner.setAdapter(adapter1);
+
+        Spinner roadConditionsSpinner = (Spinner) findViewById(R.id.roadConditionsSpinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.RoadConditions, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roadConditionsSpinner.setAdapter(adapter2);
+
+        EditText startDateTime = findViewById(R.id.startDateTime);
+        startDateTime.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(c,timeSetListener,12,12,true);
+                timePickerDialog.show();
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(c, datePickerListener, 2000, 1,1);
+                datePickerDialog.show();
+
+                return true;
+            }
+        });
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +62,26 @@ public class DrivingSessionAfter extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+
     }
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            //Do whatever you want
+        }
+    };
 
 }
