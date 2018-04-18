7bb4a1692e8be237b51ad7884eb5e88b83c69732
Florijan <klezin.florijan95@gmail.com>
2018-04-18 11:42:12 +0200

    [FK,SG,SS] Add new driving session afterwards backend setStartDateAndTime test

--- app/src/androidTest/java/com/thestreetcodecompany/roady/DrivingSessionAfterwardsUiTest.java
+++ app/src/androidTest/java/com/thestreetcodecompany/roady/DrivingSessionAfterwardsUiTest.java
@@ -2,7 +2,14 @@ package com.thestreetcodecompany.roady;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,12 +17,23 @@ import org.junit.runner.RunWith;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import junit.framework.Assert;
+
+import java.util.Calendar;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
 
 /**
@@ -30,7 +48,7 @@ import static junit.framework.Assert.assertEquals;
 @LargeTest
 public class DrivingSessionAfterwardsUiTest {
 
-    private String dateToBeEntered;
+    Calendar calendar;
 
     @Rule
     public ActivityTestRule<DrivingSessionAfter> mActivityRule =
@@ -38,7 +56,7 @@ public class DrivingSessionAfterwardsUiTest {
 
     @Before
     public void initValidString() {
-        dateToBeEntered = "12.3.2018";
+        calendar = Calendar.getInstance();
     }
 
     @Test
@@ -55,5 +73,17 @@ public class DrivingSessionAfterwardsUiTest {
         onView(withId(R.id.buttonDateStart)).perform(click());
     }
 
+    @Test
+    public void setStartDate_andStartTime() {
+        onView(withId(R.id.buttonDateStart)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2018, 4,12 ));
+        onView(withText("OK")).perform(click());
+
+        onView(withId(R.id.buttonTimeStart)).perform(click());
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(12,11));
+        onView(withText("OK")).perform(click());
+
+        Assert.assertEquals("asd","asd");
+    }
 
 }
