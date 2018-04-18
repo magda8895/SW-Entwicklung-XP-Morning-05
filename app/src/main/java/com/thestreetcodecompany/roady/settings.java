8ffa8e80edd406b677f2fbc4c0eb357ca404d28c
Unknown <alexander.perko@me.com>
2018-04-18 09:15:33 +0200

    [AP,CD] Settings backend

--- app/src/main/java/com/thestreetcodecompany/roady/settings.java
+++ app/src/main/java/com/thestreetcodecompany/roady/settings.java
@@ -6,14 +6,113 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.thestreetcodecompany.roady.classes.DBHandler;
+import com.thestreetcodecompany.roady.classes.model.DrivingSession;
+import com.thestreetcodecompany.roady.classes.model.User;
+
+import java.util.List;
+
+import static com.thestreetcodecompany.roady.classes.Helper.MakeSnackbar;
+import static com.thestreetcodecompany.roady.classes.Helper.MakeToast;
 
 /**
  * TODO: document your custom view class.
  */
-public class settings extends View {
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+        //get View Elements
+        /*final ListView listview = (ListView) findViewById(R.id.start_list);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.start_menuitem_new);
+        final FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.start_menuitem_old);
+        final TextView display = (TextView) findViewById(R.id.start_display);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);*/
+
+        //get Data
+        DBHandler dbh = new DBHandler();
+        User user = dbh.getTestUser();
+        final List<DrivingSession> sessions = dbh.getAllDrivingSessions(user);
+
+        //set List Adapter
+        listview.setAdapter(createAdapter(sessions));
+
+
+        //set Display
+        /*display.setText(user.getDriven_km() + "/" + user.getGoal_km() + " km");
+        progressBar.setMax(user.getGoal_km());
+        progressBar.setProgress(user.getDriven_km());
+
+
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MakeSnackbar("Klick Neue Fahrt",view);
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MakeToast("Klick Alte Fahrt",getApplicationContext());
+            }
+        });
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MakeToast("Klick Item: index: " + i,getApplicationContext());
+            }
+        });*/
+
+    }
+
+    public ArrayAdapter createAdapter(final List<DrivingSession> sessions){
+
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter<DrivingSession>(this,
+                R.layout.listitem_start,R.id.startitem_name, sessions) {
+            @Override
+            public View getView(int position,View convertView, ViewGroup parent) {
+                View view = super.getView(position,convertView,parent);
+
+                TextView tv_name = (TextView) view.findViewById(R.id.startitem_name);
+                TextView tv_time = (TextView) view.findViewById(R.id.startitem_date);
+                TextView tv_distance = (TextView) view.findViewById(R.id.startitem_distance);
+
+                tv_name.setText(sessions.get(position).getName());
+                tv_time.setText("" + sessions.get(position).getTimeSpan());
+                tv_distance.setText("" + sessions.get(position).getDistance() + " km");
+
+                return view;
+            }
+
+        };
+
+
+        return adapter;
+
+    }
+
     private String mExampleString; // TODO: use a default from R.string...
     private int mExampleColor = Color.RED; // TODO: use a default from R.color...
     private float mExampleDimension = 0; // TODO: use a default from R.dimen...
