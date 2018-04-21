7857ecbbd4db73648a063709b79de9761ecc4522
Christina Dionysio <christina.dionysio@student.tugraz.at>
2018-04-21 21:30:43 +0200

    [AP,CD] Settings backend missing file

--- app/src/main/java/com/thestreetcodecompany/roady/classes/DBHandler.java
+++ app/src/main/java/com/thestreetcodecompany/roady/classes/DBHandler.java
@@ -48,9 +48,13 @@ public class DBHandler extends SugarApp {
         User user = new User("Karl Heinz", 14,1000);
         user.save();
         Achievement a = new Achievement("king",1,500,"/",false,user);
-        user.save();
-        Car c = new Car("Bugatti","GU-123YEAH",user);
-        user.save();
+        a.save();
+        Car c = new Car("Bugatti","GU-123YEAH", user);
+        c.save();
+        Car c2 = new Car("Audi A6", "G-AUDI1", user);
+        c2.save();
+        Car c3 = new Car("VW Golf", "SL-234KK", user);
+        c3.save();
         CoDriver cd = new CoDriver("Carlos",user);
         cd.save();
         DrivingSession ds = new DrivingSession(true,"12-12-2012 05:21:12",2099,user);
@@ -68,8 +72,9 @@ public class DBHandler extends SugarApp {
             makeTestData();
             users = User.listAll(User.class);
         }
+        int size = users.size() - 1;
 
-        return users.get(0);
+        return users.get(size);
     }
 
     public List<DrivingSession> getAllDrivingSessions(User user)
@@ -77,9 +82,11 @@ public class DBHandler extends SugarApp {
         return DrivingSession.find(DrivingSession.class, "user = ?", "" + user.getId());
     }
 
-    public List<Car> getAllCars(User user)
-    {
-        return Car.find(Car.class, "user = ?", "" + user.getId());
-    }
+//    public List<Car> getAllCars(User user)
+//    {
+//        System.out.println("in get all cars ");
+//        //return Car.find(Car.class, "user = ?", "" + user.getId());
+//         return Car.findWithQuery(Car.class, "Select * from Car where user = ?", user.getName());
+//    }
 
 }
