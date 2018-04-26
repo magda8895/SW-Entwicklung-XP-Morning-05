dfa64c57e1f94fb5059362bc5dcfac7b1f0be7be
Unknown <gahlay@student.tugraz.at>
2018-04-26 17:26:06 +0200

    [SG,AT] Added new Features and StopWatchScreen

--- app/src/main/java/com/thestreetcodecompany/roady/NewDrivingSession.java
+++ app/src/main/java/com/thestreetcodecompany/roady/NewDrivingSession.java
@@ -3,22 +3,14 @@ package com.thestreetcodecompany.roady;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.TimePicker;
-
-import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -30,6 +22,8 @@ public class NewDrivingSession extends AppCompatActivity {
     final Calendar calNow = Calendar.getInstance();
     int buttonID = 0;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +31,14 @@ public class NewDrivingSession extends AppCompatActivity {
         setContentView(R.layout.activity_new_driving_session);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Change to StopWatchScreen
+        findViewById(R.id.StopwatchStartButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewDrivingSession.this,StopWatch.class));
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/thestreetcodecompany/roady/StopWatch.java
@@ -0,0 +1,13 @@
+package com.thestreetcodecompany.roady;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopWatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_watch);
+    }
+}
