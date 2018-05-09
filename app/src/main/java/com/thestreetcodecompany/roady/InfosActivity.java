b850343920906ea4de73a5041b94aae472e5ea05
Pangoraw <naydex.mc+github@gmail.com>
2018-05-09 10:31:25 +0200

    [FK, PB] Improve graph view barchart

--- app/src/main/java/com/thestreetcodecompany/roady/InfosActivity.java
+++ app/src/main/java/com/thestreetcodecompany/roady/InfosActivity.java
@@ -1,68 +1,244 @@
 package com.thestreetcodecompany.roady;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.orm.SchemaGenerator;
-import com.orm.SugarDb;
-import com.thestreetcodecompany.roady.classes.DBHandler;
-import com.thestreetcodecompany.roady.classes.formatters.DayAxisValueFormatter;
+import com.thestreetcodecompany.roady.classes.formatters.MonthAxisValueFormatter;
+import com.thestreetcodecompany.roady.classes.formatters.YearAxisValueFormatter;
 import com.thestreetcodecompany.roady.classes.model.DrivingSession;
 import com.thestreetcodecompany.roady.classes.model.User;
 
-import org.apache.commons.io.IOUtils;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class InfosActivity extends AppCompatActivity {
     private BarChart barChart;
-    private PieChart pieChart;
-    private PieChart pieChart2;
+    private TabLayout tabLayout;
+    private TextView timeTextView;
+    private ImageButton timeForward;
+    private ImageButton timeBack;
+
+    private TextView totalTextView;
+    private TextView averageTextView;
+    private List<TextView> weatherConditionsTextView;
+    private List<TextView> roadConditionsTextView;
+
+    private int index = 0;
+
+    public enum MODE {
+        YEAR,
+        MONTH,
+        WEEK,
+    }
+
+    private MODE mode = MODE.YEAR;
+
+    private String getTimeText(MODE mode, int index) {
+        Date date = new Date();
+        GregorianCalendar gc = new GregorianCalendar();
+        gc.setTime(date);
+
+        String pattern = "";
+        switch (mode) {
+            case YEAR:
+                gc.add(Calendar.YEAR, -index);
+                pattern = "YYYY";
+                break;
+            case MONTH:
+                pattern = "MMMM YYYY";
+                gc.add(Calendar.MONTH, -index);
+                break;
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat(pattern);
+        return dateFormat.format(gc.getTime());
+    }
+
+    private List<DrivingSession> getSessions(MODE mode, int index) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        Date startDate, endDate;
+        if (mode == MODE.MONTH) {
+            c.add(Calendar.MONTH, -index);
+            c.set(Calendar.DAY_OF_MONTH, 1);
+            c.set(Calendar.HOUR, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            startDate = c.getTime();
+            c.add(Calendar.MONTH, 1);
+            endDate = c.getTime();
+        } else {
+            c.add(Calendar.YEAR, -index);
+            c.set(Calendar.DAY_OF_YEAR, 1);
+            c.set(Calendar.HOUR, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            startDate = c.getTime();
+            c.add(Calendar.YEAR, 1);
+            endDate = c.getTime();
+        }
+
+        List<DrivingSession> sessions =  DrivingSession.getAllDrivingSessionsTimePeriod(User.getTestUser(), startDate, endDate);
+        Log.i("StartDate", startDate.toString());
+        Log.i("EndDate", endDate.toString());
+        Log.i("SessionDate", String.valueOf(sessions.size()));
+        return sessions;
+    }
+
+    private BarData getData(MODE mode) {
+        BarData barData = new BarData();
+        List<BarEntry> entries = new ArrayList<>();
+
+        Calendar c;
+        List<DrivingSession> sessions = getSessions(mode, index);
+
+        if(mode == MODE.YEAR) {
+            List<Integer> sums = new ArrayList<>(Collections.nCopies(12, 0));
+
+            for(DrivingSession session: sessions) {
+                c = Calendar.getInstance();
+                c.setTime(new Date(session.getDateTimeStart()));
+                int month = c.get(Calendar.MONTH) - 1;
+                sums.set(month, sums.get(month) + (int)(session.getDistance()));
+            }
+
+            for(int i = 0; i < sums.size(); i++) {
+                if(sums.get(i) != 0) entries.add(new BarEntry(i+1, sums.get(i)));
+            }
+        } else {
+            List<Integer> sums = new ArrayList<>(Collections.nCopies(31, 0));
+
+            for(DrivingSession session: sessions) {
+                c = Calendar.getInstance();
+                c.setTime(new Date(session.getDateTimeStart()));
+                int dayOfMonth = c.get(Calendar.DAY_OF_MONTH) - 1;
+                int month = c.get(Calendar.MONTH);
+                c.setTime(new Date());
+                c.add(Calendar.MONTH, -index);
+                int currentMonth = c.get(Calendar.MONTH);
+                if(currentMonth == month)
+                    sums.set(dayOfMonth, sums.get(dayOfMonth) + (int)(session.getDistance()));
+            }
+
+            for(int i = 0; i < sums.size(); i++) {
+                if(sums.get(i) != 0) entries.add(new BarEntry(i, sums.get(i)));
+            }
+        }
+
+        BarDataSet dataSet = new BarDataSet(entries, "Driving sessions");
+        barData.addDataSet(dataSet);
+        return barData;
+    }
+
+    private void setMode(MODE mode) {
+        index = 0;
+        timeTextView.setText(getTimeText(mode, index));
+
+        barChart.setPadding(0, 0, 0, 0);
+
+        XAxis xAxis = barChart.getXAxis();
+        if(mode == MODE.YEAR) {
+            xAxis.setLabelCount(12, true);
+            xAxis.setAxisMinimum(0);
+            xAxis.setAxisMaximum(11);
+
+            xAxis.setValueFormatter(new YearAxisValueFormatter());
+        } else if(mode == MODE.MONTH) {
+            xAxis.setLabelCount(6, true);
+            xAxis.setAxisMinimum(0);
+            xAxis.setAxisMaximum(30);
+
+            xAxis.setValueFormatter(new MonthAxisValueFormatter());
+        }
+
+        barChart.setData(getData(mode));
+        barChart.animateY(1000, Easing.EasingOption.EaseInExpo);
+        this.mode = mode;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
         barChart = findViewById(R.id.bar_chart);
+        tabLayout = findViewById(R.id.tab_layout);
+        timeTextView = findViewById(R.id.time_text_view);
+        timeBack = findViewById(R.id.time_back);
+        timeForward = findViewById(R.id.time_forward);
+        totalTextView = findViewById(R.id.total_text);
+        averageTextView = findViewById(R.id.average_text);
 
-        List<DrivingSession> sessions = DrivingSession.listAll(DrivingSession.class);
+        weatherConditionsTextView = Arrays.asList((TextView)findViewById(R.id.weather_dry), (TextView)findViewById(R.id.weather_rain), (TextView)findViewById(R.id.weather_snow), (TextView)findViewById(R.id.weather_ice));
+        roadConditionsTextView = Arrays.asList((TextView)findViewById(R.id.condition_clear), (TextView)findViewById(R.id.condition_crowd), (TextView)findViewById(R.id.condition_roadwork), (TextView)findViewById(R.id.condition_roadwork));
 
-        List<Integer> sums = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
+        timeBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(index == 0) {
+                    timeForward.setClickable(true);
+                }
+                index++;
+                timeTextView.setText(getTimeText(mode, index));
+                barChart.setData(getData(mode));
+                barChart.animateY(1000, Easing.EasingOption.EaseInExpo);            }
+        });
 
-        for(DrivingSession session: sessions) {
-            Calendar c = Calendar.getInstance();
-            //c.setTime(session.getDateTimeStart());
-            //int dayOfWeek = c.get(Calendar.DAY_OF_WEEK) - 2;
-            //sums.set(dayOfWeek, sums.get(dayOfWeek) + (int)(session.getDistance()));
-        }
+        timeForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(index <= 1) {
+                    timeForward.setClickable(false);
+                }
+                index--;
+                timeTextView.setText(getTimeText(mode, index));
+                barChart.setData(getData(mode));
+                barChart.animateY(1000, Easing.EasingOption.EaseInExpo);
+            }
+        });
 
-        List<BarEntry> entries = new ArrayList<>();
-        for(int i = 0; i < sums.size(); i++) {
-            entries.add(new BarEntry((i) % 7, sums.get(i)));
-        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                String text = null;
+                if (tab.getText() != null) {
+                    text = tab.getText().toString();
+                    if(text.toUpperCase().equals("MONAT")) {
+                        setMode(MODE.MONTH);
+                    } else {
+                        setMode(MODE.YEAR);
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         barChart.setPinchZoom(false);
         barChart.setDrawBarShadow(false);
@@ -72,56 +248,14 @@ public class InfosActivity extends AppCompatActivity {
         barChart.getDescription().setEnabled(false);
         barChart.setDoubleTapToZoomEnabled(false);
 
-        IAxisValueFormatter xAxisFormatter = new DayAxisValueFormatter();
-
         XAxis xAxis = barChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
         xAxis.setDrawGridLines(false);
         xAxis.setDrawAxisLine(false);
-        xAxis.setLabelCount(7);
-        xAxis.setValueFormatter(xAxisFormatter);
 
-        YAxis leftAxis = barChart.getAxisLeft();
-        YAxis rightAxis = barChart.getAxisRight();
+        barChart.getAxisLeft().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
 
-        leftAxis.setDrawAxisLine(false);
-        rightAxis.setDrawAxisLine(false);
-        leftAxis.setDrawGridLines(false);
-        rightAxis.setDrawGridLines(false);
-        leftAxis.setDrawLabels(false);
-        rightAxis.setDrawLabels(false);
-        leftAxis.setDrawZeroLine(false);
-        rightAxis.setDrawZeroLine(false);
-
-        BarDataSet dataSet = new BarDataSet(entries, "Driving sessions");
-        BarData data = new BarData(dataSet);
-        barChart.setData(data);
-        barChart.fitScreen();
-
-        barChart.invalidate();
-
-        DBHandler db = new DBHandler();
-        db.makeTestData();
-        User u = User.getTestUser();
-        Date start = DrivingSession.formatDateTimeDate("12-12-2012 05:21:12");
-        Date end = DrivingSession.formatDateTimeDate("12-12-2018 05:21:12");
-        List <DrivingSession> drivingSession = DrivingSession.getAllDrivingSessions(u);
-        DrivingSession.getAllDrivingSessionsTimePeriod(u,start,end);
-
-
-        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-        String output_name = "YourApp_" + df.format(new Date()) + ".db";
-        File output_file = new File(getExternalCacheDir() + File.separator + output_name);
-        try {
-            File file = new File(new SugarDb(this).getDB().getPath()); // get private db reference
-            if (file.exists() == false || file.length() == 0) throw new Exception("Empty DB");
-            IOUtils.copy(new FileInputStream(file), new FileOutputStream(output_file));
-            Intent i = new Intent(Intent.ACTION_SEND);
-            i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(output_file));
-            i.setType("*/*");
-            startActivity(Intent.createChooser(i, "Send db"));
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "Unable to export db: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+        setMode(mode);
     }
 }
